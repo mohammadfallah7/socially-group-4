5b4db34f057b6ea2a5db497570ce680b6a039513
Bardia Behdad <bardia.behdad.07@gmail.com>
2026-08-11 22:13:39 +0330

    feat: build sign up form

--- src/pages/SignUpPage.tsx
+++ src/pages/SignUpPage.tsx
@@ -1,8 +1,42 @@
+import {
+  Card,
+  CardHeader,
+  CardDescription,
+  CardTitle,
+  CardFooter,
+  CardContent,
+} from "@/components/ui/card";
+
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 const SignUpPage = () => {
   return (
-    <div>
-      <h1>Create your account</h1>
-    </div>
+    <Card>
+      <CardHeader>
+        <CardTitle>Create your account</CardTitle>
+        <CardDescription>
+          Enter your email below to create your account
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <div className="space-y-2">
+          <Label htmlFor="name">Name</Label>
+          <Input type="text" id="name" />
+        </div>
+
+        <div className="space-y-2">
+          <Label htmlFor="email">Email</Label>
+          <Input type="email" id="email" />
+        </div>
+
+        <div className="space-y-2">
+          <Label htmlFor="password">Password</Label>
+          <Input type="password" id="password" />
+        </div>
+      </CardContent>
+      <CardFooter><Button>Create Account</Button></CardFooter>
+    </Card>
   );
 };
 
