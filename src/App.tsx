1a3d325985a2a6e3a57bd3c94d3a2c70b99ecd2a
naeme <naembrm722@gmail.com>
2026-08-12 19:15:33 +0330

    feat: add root layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { Routes, Route } from "react-router";
+import RootLayout from "./components/layout/RootLayout";
+
 const App = () => {
-  return <div></div>;
+  return (
+    <Routes>
+      <Route path="/" element={<RootLayout />} />
+    </Routes>
+  );
 };
 
 export default App;
--- src/components/layout/RootLayout.tsx
+++ src/components/layout/RootLayout.tsx
@@ -1,5 +1,20 @@
+import Header from "./Header";
+import Container from "./Container";
+import { Sidebar } from "./SideBar";
+import { Outlet } from "react-router";
+
 const RootLayout = () => {
-  return <div></div>;
+  return (
+    <div className="space-y-5">
+      <Header />
+      <Container className="grid grid-cols-12">
+        <Sidebar />
+        <div className="col-span-9">
+          <Outlet />
+        </div>
+      </Container>
+    </div>
+  );
 };
 
 export default RootLayout;
