b3cae6c6dc4ebb3421538bc607154b13866cf6e2
Mohammad Fallah <mohammadfallah.w@gmail.com>
2026-08-14 23:15:54 +0330

    refactor: change some code in post card component

--- src/components/post/PostCard.tsx
+++ src/components/post/PostCard.tsx
@@ -1,6 +1,6 @@
-import { Card, CardContent } from "../ui/card";
-import { Button } from "../ui/button";
 import { Heart, MessageCircle } from "lucide-react";
+import { Button } from "../ui/button";
+import { Card, CardContent } from "../ui/card";
 
 const PostCard = () => {
   return (
@@ -13,23 +13,23 @@ const PostCard = () => {
             className="size-8 rounded-full object-cover"
           />
 
-          <div className="flex gap-4">
-            <div className="flex flex-col gap-4 lg:flex-row">
-              <span>naem-brm</span>
-              <span className="opacity-60">@naem-brm</span>
+          <div className="flex items-center gap-4">
+            <div className="flex items-center flex-col gap-4 lg:flex-row">
+              <h2 className="font-medium text-lg">naem-brm</h2>
+              <span className="text-muted-foreground">@naem-brm</span>
             </div>
-            <span className="opacity-60">about 13 hours ago</span>
+            <span className="text-muted-foreground">about 13 hours ago</span>
           </div>
         </div>
 
         <p>hellooooo</p>
 
-        <div className="flex gap-6">
-          <Button variant="ghost">
+        <div className="flex gap-6 text-muted-foreground">
+          <Button variant="ghost" className="cursor-pointer">
             <Heart /> 1
           </Button>
 
-          <Button variant="ghost">
+          <Button variant="ghost" className="cursor-pointer">
             <MessageCircle /> 0
           </Button>
         </div>
