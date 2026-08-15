2b3d303f79463af206d00fcccefc96c8a7c0c522
Mohammad Fallah <mohammadfallah.w@gmail.com>
2026-08-15 23:11:55 +0330

    feat: config react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router";
-import App from "./App.tsx";
+import { RouterProvider } from "react-router";
 import "./index.css";
+import router from "./router.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </StrictMode>,
 );
