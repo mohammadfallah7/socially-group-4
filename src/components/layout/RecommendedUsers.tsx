79ae1a53a60cd93b31f1aadf76d59c162f9c856c
Bardia Behdad <bardia.behdad.07@gmail.com>
2026-08-23 12:45:44 +0330

    feat: get recommended users

--- src/components/layout/RecommendedUsers.tsx
+++ src/components/layout/RecommendedUsers.tsx
@@ -1,70 +1,63 @@
+import { useQuery } from "@tanstack/react-query";
 import { Button } from "../ui/button";
 import { Card, CardContent } from "../ui/card";
+import { axiosInstance } from "@/lib/axios";
+import type { Response } from "@/types/response.type";
+import type { RecommendedUser } from "@/types/user.type";
 
 const RecommendedUsers = () => {
+  const { data, isPending, isError } = useQuery({
+    queryKey: ["recommended-users"],
+    queryFn: async () => {
+      const response = await axiosInstance.get<Response<RecommendedUser[]>>(
+        "/api/users/recommend",
+      );
+
+      return response.data;
+    },
+  });
+
+  if (isPending) {
+    return null;
+  }
+
+  if (isError) {
+    return null;
+  }
+
   return (
     <Card className="shadow-muted shadow-md hidden lg:block lg:col-span-4 sticky top-24">
       <CardContent className="px-6">
         <h2 className="mb-7 font-semibold text-lg">Recommended users</h2>
 
-        <div className="flex flex-col gap-5">
-          <div className="flex items-center justify-between">
-            <div className="flex items-center gap-3">
-              <img
-                src="/user_profile.svg"
-                alt="Avatar"
-                className="size-8 rounded-full object-cover"
-              />
+<div className="flex flex-col gap-5">
+  {data.data.map((user) => (
+    <div
+      key={user.id}
+      className="flex items-center justify-between"
+    >
+      <div className="flex items-center gap-3">
+        <img
+          src={user.image || "/user_profile.svg"}
+          alt="Avatar"
+          className="size-8 rounded-full object-cover"
+        />
 
-              <div>
-                <p className="text-md font-medium">sepehr</p>
-                <p className="text-sm text-muted-foreground">0 followers</p>
-              </div>
-            </div>
+        <div>
+          <p className="text-md font-medium">{user.name}</p>
 
-            <Button variant="outline" className="cursor-pointer">
-              Follow
-            </Button>
-          </div>
-
-          <div className="flex items-center justify-between">
-            <div className="flex items-center gap-3">
-              <img
-                src="/user_profile.svg"
-                alt="Avatar"
-                className="size-8 rounded-full object-cover"
-              />
-
-              <div>
-                <p className="text-md font-medium">Sephr</p>
-                <p className="text-sm text-muted-foreground">0 followers</p>
-              </div>
-            </div>
-
-            <Button variant="outline" className="cursor-pointer">
-              Follow
-            </Button>
-          </div>
-
-          <div className="flex items-center justify-between">
-            <div className="flex items-center gap-3">
-              <img
-                src="/user_profile.svg"
-                alt="Avatar"
-                className="size-8 rounded-full object-cover"
-              />
-
-              <div>
-                <p className="text-md font-medium">alireza</p>
-                <p className="text-sm text-muted-foreground">2 followers</p>
-              </div>
-            </div>
-
-            <Button variant="outline" className="cursor-pointer">
-              Follow
-            </Button>
-          </div>
+          <p className="text-sm text-muted-foreground">
+            {user._count.followers} followers
+          </p>
         </div>
+      </div>
+
+      <Button variant="outline" className="cursor-pointer">
+        Follow
+      </Button>
+    </div>
+  ))}
+</div>
       </CardContent>
     </Card>
   );
