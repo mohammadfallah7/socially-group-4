896ef839a138f6c617a0f1d06b4accabb277c361
Bardia Behdad <bardia.behdad.07@gmail.com>
2026-09-01 13:45:18 +0330

    update base url

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://socially-nextjs-six.vercel.app",
+  baseURL: "http://localhost:3000",
   withCredentials: true,
 });
