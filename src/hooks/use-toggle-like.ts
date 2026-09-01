0ef8af92db810e3002c72cd1042a1bf21ad4e84a
naeme <naembrm722@gmail.com>
2026-09-01 17:02:27 +0330

    fix: redirect unauthenticated users to sign-in on like

--- src/hooks/use-toggle-like.ts
+++ src/hooks/use-toggle-like.ts
@@ -1,21 +1,33 @@
 import { toast } from "@/components/ui/toast";
 import { axiosInstance } from "@/lib/axios";
+import { useSessionStore } from "@/stores/session.store";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useNavigate } from "react-router";
 
 export const useToggleLike = () => {
   const queryClient = useQueryClient();
+  const navigate = useNavigate();
+  const session = useSessionStore((state) => state.session);
 
   return useMutation({
     mutationFn: async (postId: string) => {
+      if (!session) {
+        navigate("/sign-in");
+        return null;
+      }
+
       const response = await axiosInstance.patch(`/api/posts/${postId}`);
 
       return response.data;
     },
 
     onSuccess: (data) => {
+      if (!data) return;
+
       queryClient.invalidateQueries({
         queryKey: ["posts"],
       });
+
       queryClient.invalidateQueries({
         queryKey: ["user-likes"],
       });
